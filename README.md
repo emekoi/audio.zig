2d23f69422f074bc6fc33c312efe3771bc48d05a
emekoi <emekankurumeh@outlook.com>
2018-11-24 01:21:35 -0600

    adding Mixer

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # *audio.zig*
 
-a multi-platform sound library for [zig](https://ziglang.org/).
+a multi-platform sound library for [zig](https://ziglang.org/) based on [cmixer](https://github.com/rxi/cmixer) and [oto](https://github.com/hajimehoshi/oto).
 
 ## TODO
 |           | windows | macosx  | linux   |
--- src/index.zig
+++ src/index.zig
@@ -4,55 +4,170 @@
 //  under the terms of the MIT license. See LICENSE for details.
 //
 
-const std = @import("std");
-const builtin = @import("builtin");
-const time = std.os.time;
+// [~] const char* cm_get_error(void);
+// [x] void cm_init(int samplerate);
+// [~] void cm_set_lock(cm_EventHandler lock);
+// [x] void cm_set_master_gain(double gain);
+// [ ] void cm_process(cm_Int16 *dst, int len);
+// [ ] cm_Source* cm_new_source(const cm_SourceInfo *info);
+// [ ] cm_Source* cm_new_source_from_file(const char *filename);
+// [ ] cm_Source* cm_new_source_from_mem(void *data, int size);
+// [ ] void cm_destroy_source(cm_Source *src);
+// [ ] double cm_get_length(cm_Source *src);
+// [ ] double cm_get_position(cm_Source *src);
+// [ ] int cm_get_state(cm_Source *src);
+// [ ] void cm_set_gain(cm_Source *src, double gain);
+// [ ] void cm_set_pan(cm_Source *src, double pan);
+// [ ] void cm_set_pitch(cm_Source *src, double pitch);
+// [ ] void cm_set_loop(cm_Source *src, int loop);
+// [ ] void cm_play(cm_Source *src);
+// [ ] void cm_pause(cm_Source *src);
+// [ ] void cm_stop(cm_Source *src);
 
-const SysPlayer = switch (builtin.os) {
-    builtin.Os.windows => @import("windows/index.zig").Player,
-    else => @panic("unsupported OS"),
+
+
+const std = @import("std");
+const Mutex = std.Mutex;
+const ArrayList = std.ArrayList;
+const Allocator = std.mem.Allocator;
+
+const math = std.math;
+pub const Player = @import("player/index.zig");
+
+const BUFFER_SIZE = 512;
+const BUFFER_MASK = BUFFER_SIZE - 1;
+
+const FX_BITS = 12;
+const FX_UNIT = 1 << FX_BITS;
+
+fn fxFromFloat(comptime T: type, f: T) isize {
+    return f * FX_UNIT;
+}
+
+fn fxLerp(comptime T: type, a: T, b: T, p: T) T {
+    return a + (((b - a) * p) >> FX_BITS);
+}
+
+fn clamp(comptime T: type, x: T, a: T, b: T) T {
+    const max = math.max(T, a, b);
+    const min = math.min(T, a, b);
+    if (x > max) {
+        return max;
+    } else if (x < min) {
+        return min;
+    } else {
+        return x;
+    }
+}
+
+pub const State = enum {
+    Stopped,
+    Playing,
+    Paused,
 };
 
-pub const Player = struct {
-    const Self = @This();
-    const Second = 1000000000;  
+pub const EventHandler = fn(Event)!void;
 
-    player: SysPlayer,
+pub const Event = union(enum) {
+    Lock: void,
+    Unlock: void,
+    Rewind: void,
+    Destroy: void,
+    Samples: []const i16,
+};
+
+
+pub const SourceInfo = struct {
+    handler: EventHandler,
     sample_rate: usize,
-    channel_count: usize,
-    bps: usize,
-    buf_size: usize,
+    length: usize,
+};
 
-    pub fn new(allocator: *std.mem.Allocator, sample_rate: usize, channel_count: usize, bps: usize, buf_size: usize) !Self {
+pub const Source = struct {
+    const Self = @This();
+
+    mixer: *Mixer,
+    next: ?*const Source,       // next source in list
+    buffer: [BUFFER_SIZE]i16,   // internal buffer with raw stereo pcm
+    handler: EventHandler,      // event handler
+    sample_rate: usize,         // stream's native samplerate
+    length: usize,              // stream's length in frames
+    end: usize,                 // end index for the current play-through
+    state: State,               // current state
+    position: i64,              // current playhead position (fixed point)
+    lgain, rgain: isize,        // left and right gain (fixed point)
+    rate: isize,                // playback rate (fixed point)
+    next_fill: isize,           // next frame idx where the buffer needs to be filled
+    loop: bool,                 // whether the source will loop when `end` is reached
+    rewind: bool,               // whether the source will rewind before playing
+    active: bool,               // whether the source is part of `sources` list
+    gain: f32,                  // gain set by `setGain()`
+    pan: f32,                   // pan set by `setPan()`
+
+    fn new(mixer: *Mixer, info: SourceInfo) Self {
+        return undefined;
+    }
+
+    fn rewindSource(self: *Self) void {
+        self.handler(Event {
+            .Rewind = {},
+        });
+        self.position = 0;
+        self.rewind = false;
+        self.end = self.length;
+        self.next_fill = 0;
+    }
+
+    fn fillBuffer(self: *Self, offset: usize, length: usize) void {
+        const start = offset;
+        const end = start + length;
+        self.handler(Event {
+            .Samples = self.buffer[start..end],
+        });
+    }
+
+    fn process(self: *Self) void {
+        const dst = self.mixer.buffer;
+
+        // do rewind if flag is set
+        if (self.rewind) {
+            self.rewindSource();
+        }
+
+        // don't process if not playing
+        switch (self.state) {
+            State.Paused => return,
+            else => {},
+        }
+
+        // process audio
+        while (length > 0) {
+            // get current position frame
+            const frame = self.position >> FX_BITS;
+        }
+    }
+};
+
+
+pub const Mixer = struct {
+    const Self = @This();
+
+    lock: Mutex,                // mutex
+    sources: ArrayList(Source), // linked list of active sources
+    buffer: [BUFFER_SIZE]i32,   // internal master buffer
+    sample_rate: isize,         // master samplerate
+    gain: isize,                // master gain (fixed point)
+
+    pub fn init(allocator: *Allocator, sample_rate: isize) Self {
         return Self {
-            .player = try SysPlayer.new(allocator, sample_rate, channel_count, bps, buf_size),
             .sample_rate = sample_rate,
-            .channel_count = channel_count,
-            .bps = bps,
-            .buf_size = buf_size,
+            .lock = Mutex.init(),
+            .sources = ArrayList.init(allocator),
+            .gain = FX_UNIT,
         };
     }
 
-    pub fn bytes_per_sec(self: *Self) usize {
-        return self.sample_rate * self.channel_count * self.bps;
-    }
-
-    pub fn write(self: *Self, data: []u8) !void {
-        var written = 0;
-        while (data.len > 0) {
-            const n = try self.player.write(data);
-            written += n;
-
-            data = data[n..];
-
-            if (data.len > 0) {
-                time.sleep(0, Second * self.buf_size / self.bytes_per_sec() / 8);
-            }
-        }
-    }
-
-    pub fn close(self: *Self) !void {
-        time.sleep(0, Second * self.buf_size / self.bytes_per_sec());
-        try self.player.close();
+    pub fn setGain(self: *Self, gain: f32) void {
+        self.gain = fxFromFloat(f32, gain);
     }
 };
